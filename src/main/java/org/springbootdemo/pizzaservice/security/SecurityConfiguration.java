fa6895cb7e2c668825336fbb2a7b978b949f5078
Jens Hoffmann <hoffi@localhost.localdomain>
2023-02-15 15:54:23 +0100

    Added controller tests

--- src/main/java/org/springbootdemo/pizzaservice/security/SecurityConfiguration.java
+++ src/main/java/org/springbootdemo/pizzaservice/security/SecurityConfiguration.java
@@ -6,16 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 //@EnableWebSecurity(debug=true)
-public class SecurityConfig {
-    @Autowired
-    private UserDetailsService userDetailsService;
+public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
